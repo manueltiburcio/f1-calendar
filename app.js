f3c0a4071c1e6bfac9330d5fefd7b4a3b8831213
Manuel Tiburcio <manuelmtiburcio@gmail.com>
2022-01-09 18:12:35 -0400

    Added loops for month and day

--- app.js
+++ app.js
@@ -1,26 +1,56 @@
-const months = document.querySelectorAll('.month-container');
-console.log(months);
+
 
 var requestOptions = {
     method: 'GET',
     redirect: 'follow',
 };
 
-    const url = "http://ergast.com/api/f1/2022.json";
+const url = "http://ergast.com/api/f1/2022.json";
 
-    async function getSchedule() {
+ async function getSchedule() {
         const response = await fetch(url, requestOptions);
         const data = await response.json();
-        console.log(data);
-        console.log(data.MRData.RaceTable.Races.length);
-        console.log(data.MRData.RaceTable.Races[0].date);
-        const races = data.MRData.RaceTable.Races;
-        races.forEach(race => {
-            console.log(race.date);
-        });
+        // console.log(data);
+        // console.log(data.MRData.RaceTable.Races.length);
+        // console.log(data.MRData.RaceTable.Races[0].date);
+        const months = document.querySelectorAll('.month-container');
+        console.log(months);
 
+        const days = document.querySelectorAll('.day-content');
+        console.log(days);
+
+        const races = data.MRData.RaceTable.Races;
+
+        races.forEach(race => {
+
+            let month = race.date.slice([5],[7]) // get month of the race
+            let day = race.date.slice([8],[10]) // get day of the race
+
+            for(let i = 0; i <= month; i++){ // check what month of the year is the race
+                if(i === month){ // enter that month
+                    for(let x = 0; x <= day; x++){ // check what day of the month selected is the race
+                         if (x === day){
+                            
+                         }
+
+                    }
+                }
+            };
+
+            console.log(day);
+            
+        });
+      
+
+       
         
-    }
+        
+}
+
+getSchedule();
+
+
+
 
 
 
@@ -32,7 +62,6 @@ var requestOptions = {
     //   .catch(error => console.log('error', error));
   
    
-    getSchedule();
     
 
 
