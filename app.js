ff0575be454a9bbbcbb8793b69570d16afc7efc0
Manuel Tiburcio <manuelmtiburcio@gmail.com>
2022-01-14 07:26:22 -0800

    Now showing race day on calendar

--- app.js
+++ app.js
@@ -10,38 +10,45 @@ const url = "http://ergast.com/api/f1/2022.json";
  async function getSchedule() {
         const response = await fetch(url, requestOptions);
         const data = await response.json();
-        // console.log(data);
-        // console.log(data.MRData.RaceTable.Races.length);
-        // console.log(data.MRData.RaceTable.Races[0].date);
+    
         const months = document.querySelectorAll('.month-container');
-        console.log(months);
-
-        const days = document.querySelectorAll('.day-content');
-        console.log(days);
 
         const races = data.MRData.RaceTable.Races;
-
+        
         races.forEach(race => {
 
-            let month = race.date.slice([5],[7]) // get month of the race
+            let month = race.date.slice([5],[7]) - 1// get month of the race
             let day = race.date.slice([8],[10]) // get day of the race
+            const days = document.querySelectorAll('.day-content');
 
-            for(let i = 0; i <= month; i++){ // check what month of the year is the race
-                if(i === month){ // enter that month
-                    for(let x = 0; x <= day; x++){ // check what day of the month selected is the race
-                         if (x === day){
-                            
-                         }
-
-                    }
-                }
-            };
-
+            console.log(month);
             console.log(day);
+
+            let day_365 = 0; // Translates month - day format to 365 date format
+
+            for (let i = 0; i <= month; i++){
+                
+                let currentMonth = document.querySelector("[data-month-id="+CSS.escape(i)+"]");
+                let currentDay = currentMonth.querySelectorAll('.day-content');
+
+                if (i == month){
+                
+                      day_365 += parseInt(day);
+                      days[day_365].classList.add("active");
+                      
+                      
+                }else{
+
+                    day_365 += currentDay.length;
+                    
+                }
+             
+            }
+            console.log(day_365);
             
         });
       
-
+      
        
         
         
