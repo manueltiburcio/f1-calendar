3cceba075783a7bcdbb36fdebe7a784bb457ea83
Manuel Tiburcio <manuelmtiburcio@gmail.com>
2023-08-08 07:44:55 -0700

    Updated season

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var requestOptions = {
     redirect: 'follow',
 };
 
-const url = "https://ergast.com/api/f1/2022.json";
+const url = "https://ergast.com/api/f1/2023.json";
 const modal = document.querySelector(".modal");
 
 //-----------// Track Images //-----------//
@@ -80,67 +80,67 @@ imgArray[21].src = 'images/22.png';
 imgArray[22] = new Image();
 imgArray[22].src = 'images/23.png';
 
- async function getSchedule() {
-        const response = await fetch(url, requestOptions);
-        const data = await response.json();
-    
-        const months = document.querySelectorAll('.month-container');
+async function getSchedule() {
+    const response = await fetch(url, requestOptions);
+    const data = await response.json();
 
-        const races = data.MRData.RaceTable.Races;
-        console.log(races)
-        
-        races.forEach(race => {
+    const months = document.querySelectorAll('.month-container');
 
-            let month = race.date.slice([5],[7]) - 1// get month of the race
-            let day = race.date.slice([8],[10]) - 1 // get day of the race
-            const days = document.querySelectorAll('.day-content');
+    const races = data.MRData.RaceTable.Races;
+    console.log(races)
 
-            //console.log(race.raceName)
-            //console.log(month);
-            //console.log(day);
+    races.forEach(race => {
 
-            let day_365 = 0; // Translates month - day format to 365 date format
+        let month = race.date.slice([5], [7]) - 1// get month of the race
+        let day = race.date.slice([8], [10]) - 1 // get day of the race
+        const days = document.querySelectorAll('.day-content');
 
-            for (let i = 0; i <= month; i++){
-                
-                let currentMonth = document.querySelector("[data-month-id="+CSS.escape(i)+"]");
-                let currentDay = currentMonth.querySelectorAll('.day-content');
+        //console.log(race.raceName)
+        //console.log(month);
+        //console.log(day);
 
-                if (i == month){
-                
-                      day_365 += parseInt(day);
-                      days[day_365].classList.add("active");
-                      days[day_365].classList.add("open");
-                      
-                      
-                }else{
+        let day_365 = 0; // Translates month - day format to 365 date format
 
-                    day_365 += currentDay.length;
-                    
-                }
-             
+        for (let i = 0; i <= month; i++) {
+
+            let currentMonth = document.querySelector("[data-month-id=" + CSS.escape(i) + "]");
+            let currentDay = currentMonth.querySelectorAll('.day-content');
+
+            if (i == month) {
+
+                day_365 += parseInt(day);
+                days[day_365].classList.add("active");
+                days[day_365].classList.add("open");
+
+
+            } else {
+
+                day_365 += currentDay.length;
+
             }
 
+        }
 
-        // document.querySelectorAll('.open').forEach(item => 
+
+        // document.querySelectorAll('.open').forEach(item =>
         //         item.addEventListener('click', event => {
         //           console.log(event.target)
         //      })
         //    )
 
-            //  modal.innerHTML = 
-            //   `<h1> ${race.raceName}</h1>
-            //     <p>${race.time}</p>
-            //     <button id="close">Close</button>
-              
-            //   `;
-              
+        //  modal.innerHTML =
+        //   `<h1> ${race.raceName}</h1>
+        //     <p>${race.time}</p>
+        //     <button id="close">Close</button>
 
-            //console.log(modal.innerHTML);
-            
-        });
+        //   `;
 
-        modal.innerHTML = 
+
+        //console.log(modal.innerHTML);
+
+    });
+
+    modal.innerHTML =
         ` <h1>definition</h1>
           <img src="">
           <h4>My Time:</h4>
@@ -148,61 +148,61 @@ imgArray[22].src = 'images/23.png';
           <h4>Track Time:</h4>
           <p>definition</p>
           <button id="close">Close</button>
-        
+
         `;
 
-        const modalContainer = document.getElementById('modal-container');
-        let close = document.getElementById('close');
-        
-        document.querySelectorAll('.open').forEach(item => {
-            item.addEventListener('click', event => {
-                modalContainer.classList.add('show');
+    const modalContainer = document.getElementById('modal-container');
+    let close = document.getElementById('close');
 
-                let clickedDay = parseInt(event.target.innerHTML);
-                console.log(clickedDay);
+    document.querySelectorAll('.open').forEach(item => {
+        item.addEventListener('click', event => {
+            modalContainer.classList.add('show');
 
-                let clickedMonth = parseInt(event.target.parentNode.parentNode.parentNode.parentNode.getAttribute("data-month-id"));
-                
-                console.log(clickedMonth);
+            let clickedDay = parseInt(event.target.innerHTML);
+            console.log(clickedDay);
 
+            let clickedMonth = parseInt(event.target.parentNode.parentNode.parentNode.parentNode.getAttribute("data-month-id"));
 
-                races.forEach(race => {
-
-                    let monthLookUp = race.date.slice([5],[7]) - 1// get month of the race
-                    let dayLookUp = race.date.slice([8],[10])// get day of the race
-
-                    //console.log("clickedDay " + clickedDay);
-                    //console.log("clickedMonth " + clickedMonth);
-
-                    //console.log("monthLookUp" + monthLookUp);
-                    //console.log("dayLookUp "+ dayLookUp);
+            console.log(clickedMonth);
 
 
-                    if ( (monthLookUp == clickedMonth) && (dayLookUp == clickedDay) ){
-                        modal.getElementsByTagName('h1')[0].innerHTML = race.raceName;
-                        let raceImg = parseInt(race.round) - 1;
-                        console.log("race.round " + race.round);
-                        console.log("race photo " + imgArray[raceImg]);
+            races.forEach(race => {
 
-                        modal.getElementsByTagName('img')[0].src = imgArray[raceImg].src;
-                        modal.getElementsByTagName('p')[1].innerHTML = race.time.slice([0],[5]);
-                    }
+                let monthLookUp = race.date.slice([5], [7]) - 1// get month of the race
+                let dayLookUp = race.date.slice([8], [10])// get day of the race
+
+                //console.log("clickedDay " + clickedDay);
+                //console.log("clickedMonth " + clickedMonth);
+
+                //console.log("monthLookUp" + monthLookUp);
+                //console.log("dayLookUp "+ dayLookUp);
+
+
+                if ((monthLookUp == clickedMonth) && (dayLookUp == clickedDay)) {
+                    modal.getElementsByTagName('h1')[0].innerHTML = race.raceName;
+                    let raceImg = parseInt(race.round) - 1;
+                    console.log("race.round " + race.round);
+                    console.log("race photo " + imgArray[raceImg]);
+
+                    modal.getElementsByTagName('img')[0].src = imgArray[raceImg].src;
+                    modal.getElementsByTagName('p')[1].innerHTML = race.time.slice([0], [5]);
+                }
 
 
                 // modal.getElementsByTagName('h1')[0].innerHTML = clickedDay;
                 // modal.getElementsByTagName('h1')[1].innerHTML = clickedMonth;
-                
+
             });
 
-            
-          })
-         
 
-        close.addEventListener('click',() => {
+        })
+
+
+        close.addEventListener('click', () => {
             modalContainer.classList.remove('show');
-        });    
-        
-});
+        });
+
+    });
 
 }
 
@@ -212,7 +212,7 @@ getSchedule();
 
 
 
- 
+
 
 // Modal
 // let open = document.querySelectorAll('.open');
@@ -232,13 +232,13 @@ getSchedule();
 // });
 
 // for (var i = 0 ; i < open.length; i++) {
-//     open[i].addEventListener('click' , openModal, false ) ; 
+//     open[i].addEventListener('click' , openModal, false ) ;
 //  }
 
 
 
 
-    
+
 
 
 
